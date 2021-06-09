d967a6a81efb6203408caabb1aba336ac516b051
Karim Mk <mk47blast@protonmail.com>
2021-06-09 19:28:11 +0100

    Add private_dot_config/zsh/aliases/aliases.zsh

---
+++ private_dot_config/zsh/aliases/aliases.zsh
@@ -0,0 +1,32 @@
+alias ls='ls --color=auto'
+alias grep='grep --colour=auto'
+alias egrep='egrep --colour=auto'
+alias fgrep='fgrep --colour=auto'
+
+alias cp="cp -i"                          # confirm before overwriting something
+alias df='df -h'                          # human-readable sizes
+alias free='free -m'                      # show sizes in MB
+alias more=less
+
+# Pacman-alias
+alias p='sudo pacman'
+alias pi='sudo pacman -S'
+alias pu='sudo pacman -Syu'
+alias pr='sudo pacman -Rs'
+
+# Paru-alias
+alias y='paru'
+alias yr='paru -Rs'
+
+# Apps-alias
+alias v='nvim'
+alias c='clear'
+alias q='exit'
+alias h='htop'
+alias chz='chezmoi'
+
+#Protonvpn aliases
+alias pvc='sudo protonvpn c'
+alias pvd='sudo protonvpn d'
+alias pvs='sudo protonvpn s'
+alias pvr='sudo protonvpn reconnect'
